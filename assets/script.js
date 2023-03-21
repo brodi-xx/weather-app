4511124f08d533a3c2403c7864c9f1d856177829
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-03-20 19:16:15 -0500

    added in local storage

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,28 @@
 //weather api url plus search function for city
-    let weather = {
+
+      let weather = {
         apiKey: "c049ffc93d7825412d943b36b0223607",
         fetchWeather: function (city) {
           fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" +
               city +
               "&units=imperial&appid=" +
-              this.apiKey)
-              //alert if no location found with alert
+              this.apiKey
+          )
+        //alert if no location found with alert
             .then((response) => {
               if (!response.ok) {
                 alert("No weather found.");
-                throw new Error("No weather found.");}
-              return response.json();})
-            .then((data) => this.displayWeather(data));},
+                throw new Error("No weather found.");
+              }
+              return response.json();
+            })
+            .then((data) => {
+              this.displayWeather(data);
+        // Save the last searched city in local storage
+              localStorage.setItem("lastCity", city);
+            });
+        },
         displayWeather: function (data) {
           const { name } = data;
           const { icon, description } = data.weather[0];
@@ -28,20 +37,30 @@
             "Humidity: " + humidity + "%";
           document.querySelector(".wind").innerText =
             "Wind: " + Math.round(speed) + " mph";
-          document.querySelector(".weather").classList.remove("loading");},
+          document.querySelector(".weather").classList.remove("loading");
+          document.body.style.backgroundImage =
+            "url('https://source.unsplash.com/1600x900/?wallpaper')";
+        },
         search: function () {
-          this.fetchWeather(document.querySelector(".search-bar").value);},
+          this.fetchWeather(document.querySelector(".search-bar").value);
+        },
       };
+      
+    // Load the last searched city from local storage
+      const lastCity = localStorage.getItem("lastCity");
+      if (lastCity) {
+        weather.fetchWeather(lastCity);
+      } else {
+        weather.fetchWeather("Austin"); // Default city
+      }
     //click function for searching location
       document.querySelector(".search button").addEventListener("click", function () {
-        weather.search();}
-        );
+        weather.search();
+      });
     //enter function for searching
-      document
-        .querySelector(".search-bar")
-        .addEventListener("keyup", function (event) {
-          if (event.key == "Enter") {
-            weather.search();}}
-            );
-    //fetch weather for Austin as default
-      weather.fetchWeather("Austin");
+      document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+          weather.search();
+        }
+      });
+      
